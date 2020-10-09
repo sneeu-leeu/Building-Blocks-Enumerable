7ba8a2bd2c6640e0855e9a1f243be3f745f1ea1a
CornerstoneII <seunmyles@gmail.com>
2020-10-09 12:05:54 +0100

    Add my_all

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -29,9 +29,25 @@ module Enumerable
       select
     end
   end
-  
-    
- 
+
+  def my_all(arg = nil)
+    if block_given?
+      my_each.to_a {|idx| return false if yield(idx) == false}
+      return true
+    elsif arg.nil?
+      my_each.to_a {|idx| return false if idx = false or if idx.nil?}
+    elsif !arg.nil && (arg.is_a? class)
+      my_each.to_a {|idx| return false unless [item.class, item.class.superclass].include?(arg)}
+    elsif !arg.nil && arg.class == regexp
+      my_each.to_a {|idx| return false unless arg.match(idx)}
+    else
+      my_each.to_a {|idx| return false if idx != arg}
+    end
+    true
+  end
+
+
+
 
 
 # rubocop: enable Style/CaseEquality
