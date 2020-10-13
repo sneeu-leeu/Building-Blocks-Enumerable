5ac7a5cedb11e5c9b2031766fbc4621d43e165f5
Stephan Annandale <sa070197@gmail.com>
2020-10-13 10:49:21 +0200

    Fix errors in my_each and my_each_idx

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,79 +1,63 @@
-# rubocop: disable Metrics/CyclomaticComplexity
 module Enumerable # rubocop:todo Metrics/ModuleLength
-  def range_return_array(idx)
-    if instance_of?(Range)
-      self
-    else
-      idx
-    end
-  end
+  def my_each(&block)
+    return unless block_given?
 
-  def my_each
-    return to_enum unless block_given?
-
-    elements = to_a
-    elements.size.times do |n|
-      yield elements[n]
-    end
-    range_return_array(idx)
+    each(&block)
   end
 
   def my_each_with_index
-    return to_enum unless block_given?
+    return unless block_given?
 
-    elements = to_a
-    elements.size.times do |n|
-      yield elements[n], n
+    idx = 0
+    each do |i|
+      yield i, idx
+      idx += 1
     end
-    range_return_array(idx)
   end
 
   def my_select
-    return to_enum unless block_given?
+    return to_enum(:my_select) unless block_given?
 
-    value = to_a
-    selectArr = [] # rubocop:todo Naming/VariableName
-    value.my_each do |value| # rubocop:todo Lint/ShadowingOuterLocalVariable
-      selectArr << value if yield(value) # rubocop:todo Naming/VariableName
+    my_arr = []
+    my_each { |item| my_arr << item if yield item }
+    my_arr
+  end
+
+  # rubocop:todo Metrics/CyclomaticComplexity
+  def my_all?(arguement = nil) # rubocop:todo Metrics/PerceivedComplexity
+    if block_given?
+      to_a.my_each { |items| return false if yield(items) == false }
+      return true
+    elsif arguement.nil?
+      to_a.my_each { |items| return false if items == false || items.nil? }
+    elsif !arguement.nil? && (arguement.is_a? Class)
+      to_a.my_each { |item| return false unless [item.class, item.class.superclass].include?(parameter) }
+    elsif !arguement.nil? && arguement.instance_of?(Regexp)
+      to_a.my_each { |item| return false unless arguement.match(item) }
+    else
+      to_a.my_each { |item| return false if item != arguement }
     end
-    selectArr # rubocop:todo Naming/VariableName
+    true
   end
+  # rubocop:enable Metrics/CyclomaticComplexity
 
-  # rubocop:todo Metrics/PerceivedComplexity
-  def my_all?(args = nil) # rubocop:todo Metrics/CyclomaticComplexity
-  if block_given? # rubocop:todo Layout/IndentationWidth
-    to_a.my_each { |items| return false if yield(items) == false }
-    return true
-  elsif args.nil?
-    to_a.my_each { |items| return false if items == false || items.nil? }
-  elsif !args.nil? && (args.is_a? Class)
-    to_a.my_each { |item| return false unless [item.class, item.class.superclass].include?(parameter) }
-  elsif !args.nil? && args.instance_of?(Regexp)
-    to_a.my_each { |item| return false unless args.match(item) }
-  else
-    to_a.my_each { |item| return false if item != args }
-  end
-  true
-  end
-
-  # rubocop:enable Metrics/PerceivedComplexity
-  # rubocop:todo Metrics/PerceivedComplexity
-  def my_any?(args = nil) # rubocop:todo Metrics/CyclomaticComplexity
+  # rubocop:todo Metrics/CyclomaticComplexity
+  def my_any?(arguement = nil) # rubocop:todo Metrics/PerceivedComplexity
     if block_given?
       to_a.my_each { |items| return true if yield(items) }
       return false
-    elsif args.nil?
+    elsif arguement.nil?
       to_a.my_each { |items| return true if items }
-    elsif !args.nil? && (args.is_a? Class)
+    elsif !arguement.nil? && (arguement.is_a? Class)
       to_a.my_each { |items| return true if [items.class, items.class.superclass].include?(parameter) }
-    elsif !args.nil? && args.instance_of?(Regexp)
-      to_a.my_each { |items| return true if args.match(items) }
+    elsif !arguement.nil? && arguement.instance_of?(Regexp)
+      to_a.my_each { |items| return true if arguement.match(items) }
     else
-      to_a.my_each { |items| return true if items == args }
+      to_a.my_each { |items| return true if items == arguement }
     end
     false
   end
-  # rubocop:enable Metrics/PerceivedComplexity
+  # rubocop:enable Metrics/CyclomaticComplexity
 
   def my_none?(arg = nil, &block)
     !my_any?(arg, &block)
@@ -83,12 +67,12 @@ module Enumerable # rubocop:todo Metrics/ModuleLength
     idx = to_a
     count = 0
     if arg
-      idx.my_each do |element|
-        count += 1 if element == arg
+      idx.my_each do |n|
+        count += 1 if n == arg
       end
     elsif block_given?
-      idx.my_each do |element|
-        count += 1 if (yield element) == true
+      idx.my_each do |n|
+        count += 1 if (yield n) == true
       end
     else
       idx.my_each do
@@ -98,65 +82,48 @@ module Enumerable # rubocop:todo Metrics/ModuleLength
     count
   end
 
-  def my_map(arg = nil)
-    return to_enum if !block_given? && !arg
+  def my_map(proc = nil)
+    return to_enum(:my_map) unless block_given? || !proc.nil?
 
-    elements = clone.to_a
-    elements.my_each_with_index do |element, i|
-      elements[i] = if arg
-                      arg.call(element)
-                    else
-                      yield element
-                    end
+    new_array = []
+    if proc.nil?
+      to_a.my_each { |items| new_array << yield(items) }
+    else
+      to_a.my_each { |items| new_array << proc.call(items) }
     end
-    elements
+    new_array
   end
 
   # rubocop:todo Metrics/PerceivedComplexity
-  # rubocop:todo Metrics/MethodLength
-  def my_inject(arg_first = nil, sym = nil) # rubocop:todo Metrics/CyclomaticComplexity
-    elements = to_a
-    if arg_first && sym
-      arg = sym
-      accum = arg_first
-      elements.my_each_with_index do |element, _i|
-        accum = accum.send(arg, element)
-      end
-      return accum
-    elsif arg_first && !sym
-      arg = arg_first
-    elsif !arg_first && !sym
-      accum = nil
-      elements.my_each do |element|
-        accum = if accum.nil?
-                  element
-                else
-                  yield accum, element
-                end
-      end
-    end
-    if arg.instance_of?(Symbol)
-      accum = nil
-      elements.my_each do |element|
-        accum = if accum.nil?
-                  element
-                else
-                  accum.send(arg, element)
-                end
-      end
-    elsif block_given? && arg
-      accum = arg
-      elements.my_each do |element|
-        accum = yield accum, element
+  # rubocop:todo Metrics/CyclomaticComplexity
+  def my_inject(*args) # rubocop:todo Metrics/MethodLength
+    if args.size == 2
+      raise TypeError, "#{args[1]} is not a symbol" unless args[1].is_a?(Symbol)
+
+      my_each { |item| args[0] = args[0].send(args[1], item) }
+      args[0]
+    elsif args.size == 1 && !block_given?
+      raise TypeError, "#{args[0]} is not a symbol" unless args[0].is_a?(Symbol)
+
+      memo = first
+      drop(1).my_each { |item| memo = memo.send(args[0], item) }
+      memo
+    elsif args.empty? && !block_given?
+      raise LocalJumpError, 'no block given'
+    else
+      memo = args[0] || first
+      if memo == args[0]
+        my_each { |item| memo = yield(memo, item) if block_given? }
+        memo # rubocop:todo Style/IdenticalConditionalBranches
+      else
+        drop(1).my_each { |item| memo = yield(memo, item) if block_given? }
+        memo # rubocop:todo Style/IdenticalConditionalBranches
       end
     end
-    accum
   end
-  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/CyclomaticComplexity
   # rubocop:enable Metrics/PerceivedComplexity
 end
-
-# rubocop: enable Metrics/CyclomaticComplexity
 
 def multiply_els(arr)
   p arr.my_inject(1) { |d, v| d * v }
